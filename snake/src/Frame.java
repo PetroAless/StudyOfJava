3f604ea743499b24b5b9a35b0e357a3220041118
Alessio <alessio.petrotta1@gmail.com>
2023-03-20 18:07:16 +0100

    added basic comments, added btn with listener, setting up easy functions, hotfix

--- snake/src/Frame.java
+++ snake/src/Frame.java
@@ -1,14 +1,20 @@
 import javax.swing.*;
-public class Frame extends JFrame{
-    JFrame frame;
-    Frame(int width,int height){
+
+
+public class Frame extends JFrame{//setting up a frame class with my functions to work with jframe and else
+    JFrame frame;//basic frame
+    Frame(int width,int height){ //easy constructor with size
         frame = new JFrame("Frame Demo");
         frame.setSize(width,height);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //setting closing operation
     }
 
-    void add(JComponent c){
+    void add(JComponent c){  //function to simply add a component easily, withtout writing f.frame.add(c)
         frame.add(c);
+    }
+    void render(){  //function to just render and other things to do at the end
+        frame.setLayout(null);
+        frame.setVisible(true);
     }
     public static void main(String[] args) {
         Frame f = new Frame(1200,900);
@@ -17,12 +23,17 @@ public class Frame extends JFrame{
         JTextField text = new JTextField("testo di prova");
         text.setBounds(500,200,200,80);
 
+
         JButton btn = new JButton("button");
         btn.setBounds(550,300,100,40);
+        btn.addActionListener(e-> {  //add event action listener, creating nested functions? idk lmao
+            text.setText("IL BOTTONE FUNZIONA");
+        });
 
-        f.add(text);
+
+        f.add(text);//add elements
         f.add(btn);
-        f.frame.setLayout(null);
-        f.frame.setVisible(true);
+
+        f.render();//show the frame
     }
 }
