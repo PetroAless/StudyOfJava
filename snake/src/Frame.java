b5f7201fce66f15114833642792ebe45dea79337
Alessio <alessio.petrotta1@gmail.com>
2023-03-20 18:46:40 +0100

    trying with imageIcon

--- snake/src/Frame.java
+++ snake/src/Frame.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 
 public class Frame extends JFrame{//setting up a frame class with my functions to work with jframe and else
@@ -20,20 +21,28 @@ public class Frame extends JFrame{//setting up a frame class with my functions t
         Frame f = new Frame(1200,900);
 
 
-        JTextField text = new JTextField("testo di prova");
-        text.setBounds(500,200,200,80);
+        JLabel label = new JLabel("testo di prova");
+        label.setBounds(500,200,200,80);
+        JLabel img = new JLabel(new ImageIcon("heart.png"));
+
+
+
 
 
         JButton btn = new JButton("button");
         btn.setBounds(550,300,100,40);
         btn.addActionListener(e-> {  //add event action listener, creating nested functions? idk lmao
-            text.setText("IL BOTTONE FUNZIONA");
+            label.setText("Giorgia ti amo tanto tanto");
+            label.setBackground(Color.PINK);
+            label.setOpaque(true);
+
         });
 
 
-        f.add(text);//add elements
+        f.add(label);//add elements
         f.add(btn);
-
+        f.frame.add(img);
+        f.pack();
         f.render();//show the frame
     }
 }
