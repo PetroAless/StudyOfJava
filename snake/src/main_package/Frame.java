5ea58873b5d04e544b50e44f06cad97213649842
Alessio <alessio.petrotta1@gmail.com>
2023-03-22 23:16:35 +0100

    added dynamism of s.body, added move() for following the previous nodes, added initializeNewNode(), minor fix to checkCollision(), minor fixes in Frame

--- snake/src/main_package/Frame.java
+++ snake/src/main_package/Frame.java
@@ -26,9 +26,10 @@ public class Frame extends JFrame implements ActionListener {//setting up a fram
     }
     void prepareSnake(){//for cleaner code, preparing snake's attributes
         s = new Snake();
-        s.setBounds();
         panel.add(s.head);
-        panel.add(s.body[0]);
+        for (int i = 0; i < s.bodyN; i++) {
+            panel.add(s.body[i]);
+        }
     }
 
     void setPanel (Color colorOrNull){//preparing panel and all parts
@@ -59,7 +60,6 @@ public class Frame extends JFrame implements ActionListener {//setting up a fram
         int x = r.nextInt(f.getWidth()-10);
         int y = r.nextInt(f.getHeight()-10);
         this.apple.setLocation(x,y);
-
     }
 
 
@@ -69,62 +69,70 @@ public class Frame extends JFrame implements ActionListener {//setting up a fram
         f.setVisible(true);
     }
 
-    public boolean checkCollision(){ //
-        int tmp = 0;
-        if(s.top)tmp=1;
-        if(s.right)tmp=2;
-        if(s.bottom)tmp=3;
-        if(s.left)tmp=4;
-        switch(tmp){
-            case 1:
-        if(
-                ( this.s.hy -5 <= this.apple.getY() +5 && this.s.hy -5 >= this.apple.getY() )
-                    &&
-                ( this.s.hx == this.apple.getX() )
-        )
-                return true;
-                break;
-            case 2:
-        if(
+    public boolean checkCollision(){
+        boolean res = false;
+        switch(s.d){
+            case up-> {
+                if (
+                        (this.s.hy - 5 <= this.apple.getY() + 5 && this.s.hy - 5 >= this.apple.getY())
+                                &&
+                        (this.s.hx == this.apple.getX())
+                )
+                    res = true;
+            }
+            case right-> {
+                if (
 
-                ( this.s.hx +5 <= this.apple.getX() && this.s.hx +5 >= this.apple.getX() -5 )
-                    &&
-                ( this.s.hy == this.apple.getY() )
+                        (this.s.hx + 5 <= this.apple.getX() && this.s.hx + 5 >= this.apple.getX() - 5)
+                                &&
+                        (this.s.hy == this.apple.getY())
 
-        )
-                return true;
-                break;
-            case 3:
-        if(
-                ( this.s.hy +5 <= this.apple.getY() && this.s.hy +5 >= this.apple.getY() -5 )
-                    &&
-                ( this.s.hx == this.apple.getX() )
-        )
-                return true;
-                break;
-            case 4:
-        if(
-                ( this.s.hx -5 <= this.apple.getX() +5 && this.s.hx -5 >= this.apple.getX() )
-                    &&
-                ( this.s.hy == this.apple.getY() )
-        )
-                return true;
-                break;
-            default :
+                )
+                res = true;
+            }
+
+            case down-> {
+                if (
+                        (this.s.hy + 5 <= this.apple.getY() && this.s.hy + 5 >= this.apple.getY() - 5)
+                                &&
+                        (this.s.hx == this.apple.getX())
+                )
+                    res = true;
+            }
+
+            case left-> {
+                if (
+                        (this.s.hx - 5 <= this.apple.getX() + 5 && this.s.hx - 5 >= this.apple.getX())
+                                &&
+                        (this.s.hy == this.apple.getY())
+                )
+                    res = true;
+            }
+
+            default -> {
                 System.out.println("error, collision checking in default mode");
-                break;
+            }
         }
-        return false;
+        if(res){
+            System.out.println("COLLISION");
+            s.bodyN++;
+            s.initializeNewNode();
+            panel.add(s.body[s.bodyN-1]);
+        }
+
+        return res;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        this.s.setXY(this.s.hx,this.s.hy);
+
+        this.s.move();
         if(checkCollision()){
             this.randomizePositionOfApple();
+            this.s.changeDirection();
         }
         repaint();
-        this.s.hy+=5;
+
     }
 
 
--- snake/src/main_package/Snake.java
+++ snake/src/main_package/Snake.java
@@ -2,20 +2,99 @@ package main_package;
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 public class Snake {
+    public enum direction {up,right,down,left};
     JLabel head, body[] = new JLabel[900];
-    boolean left = false,right = false,top = false,bottom = true;
-    int hx,hy,bx,by;//head x, head y, body x, body y
+    direction d = direction.down;
+
+    int hx,hy;//head x, head y
+    int bodyN = 3;//number of bodies
+
     Snake(){
         head = new JLabel(new ImageIcon("src/resources/head.png"));
-        body[0] = new JLabel(new ImageIcon("src/resources/dot.png"));
+        for (int i = 0; i < bodyN; i++) {
+            body[i] = new JLabel(new ImageIcon("src/resources/dot.png"));
+        }
+        setBounds();
     }
     public void setBounds(){
         head.setBounds(0,0,10,10);
-        body[0].setBounds(10,0,10,10);
+        for (int i = 0; i < this.bodyN; i++) {
+            body[i].setBounds(0,0,10,10);
+        }
     }
-    public void setXY(int x, int y){
-        head.setLocation(x,y);
-        body[0].setLocation(head.getX()-10,head.getY());
+
+    public void move(){
+        int beforeMove,tmp;
+        switch (this.d){
+            case up -> {
+                tmp = this.hy+5;
+                this.hy-=5;
+                this.head.setLocation(this.hx,this.hy);
+                for(int i = 0; i < this.bodyN; i++){
+                    beforeMove = body[i].getY()+5;
+                    body[i].setLocation(body[i].getX(),tmp);
+                    tmp = beforeMove;
+
+                }
+            }
+            case right -> {
+                tmp = this.hx-5;
+                this.hx+=5;
+                this.head.setLocation(this.hx,this.hy);
+                for(int i = 0; i < this.bodyN ; i++){
+                    System.out.println(i);
+                    beforeMove = body[i].getX()-5;
+                    body[i].setLocation(tmp,body[i].getY());
+                    tmp = beforeMove;
+
+                }
+            }
+            case down -> {
+                tmp = this.hy-5;
+                this.hy+=5;
+                this.head.setLocation(this.hx,this.hy);
+                for(int i = 0; i < this.bodyN ; i++){
+                    beforeMove = body[i].getY()-5;
+                    body[i].setLocation(body[i].getX(),tmp);
+                    tmp = beforeMove;
+
+                }
+            }
+            case left -> {
+                tmp = this.hx+5;
+                this.hx-=5;
+                this.head.setLocation(this.hx,this.hy);
+                for(int i = 0; i < this.bodyN ; i++){
+                    beforeMove = body[i].getX()+5;
+                    body[i].setLocation(tmp,body[i].getY());
+                    tmp = beforeMove;
+
+                }
+            }
+            default -> {
+                System.out.println("error, default in move()");
+            }
+        }
+    }
+    public void initializeNewNode(){
+        this.body[bodyN-1] = new JLabel(new ImageIcon("src/resources/dot.png"));
+        switch (this.d){
+            case up -> {
+                this.body[bodyN-1].setBounds(this.body[bodyN-2].getX(),this.body[bodyN-2].getY()+10,10,10);
+            }
+            case right -> {
+                this.body[bodyN-1].setBounds(this.body[bodyN-2].getX()-10,this.body[bodyN-2].getY(),10,10);
+            }
+            case down -> {
+                this.body[bodyN-1].setBounds(this.body[bodyN-2].getX(),this.body[bodyN-2].getY()-10,10,10);
+            }
+            case left -> {
+                this.body[bodyN-1].setBounds(this.body[bodyN-2].getX()+10,this.body[bodyN-2].getY(),10,10);
+            }
+        }
+    }
+    public void changeDirection(){ //@todo fix change of X IF CHANGING FROM TOP-DOWN TO LEFT-RIGHT (AND CONSEQUENCES)
+        this.d = direction.right;
     }
     public void startGame(){
 
